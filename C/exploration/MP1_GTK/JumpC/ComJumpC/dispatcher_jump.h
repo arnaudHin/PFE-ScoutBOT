a0b4a7f79211adeb5e386a6af2bfbb6e0b8dc40c
Adrien-PC <adrien.leroux03@gmail.com>
2021-11-26 14:38:35 +0100

    [EXPLO] Gtk new version

---
+++ C/exploration/MP1_GTK/JumpC/ComJumpC/dispatcher_jump.h
@@ -0,0 +1,34 @@
+// /**
+//  * @file  dispatcher_pocket.h
+//  * @author Montet Julien & Marbeuf Pierre
+//  * @version 3.0
+//  * @date 12/05/2020
+//  * @brief dispatcher pocket
+//  */
+
+// #ifndef SRC_POCKET_COM_POCKET_DISPATCHER_JUMP_H_
+// #define SRC_POCKET_COM_POCKET_DISPATCHER_JUMP_H_
+
+
+// /************************ PROTOTYPE DECLARATION **************************************/
+
+// /** \fn static void Pilot_start ()
+//  *  \brief Function dedicated to start the dispatcher and postman (Initialize and open the mailbox + create a thread)
+//  */
+// extern void dispatcher_jump_start();
+
+// /**
+//  *  \fn static void new_dispatcher_jump_start()
+//  *
+//  *  \brief Function dedicated to start the dispatcher  with pilot and without postman.
+//  */
+// extern void new_dispatcher_jump_start();
+
+// /** \fn extern void Pilot_set_direction(Direction dir)
+//  *  \brief Function dedicated to wait the end of thread to stop the dispatcher
+//  */
+// extern void dispatcher_jump_stop();
+
+// /************************ END PROTOTYPE DECLARATION **********************************/
+
+// #endif /* SRC_POCKET_COM_POCKET_DISPATCHER_JUMP_H_ */
