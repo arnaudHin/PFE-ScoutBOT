3844bf33325f76b4f481206020219c2d7adf197f
MartinArondel <martin-arondel@hotmail.fr>
2021-12-16 10:51:06 +0100

    [CREAT] add I2C and MPU6050 files

--- C/production/Race/CM4/Core/Inc/config.h
+++ C/production/Race/CM4/Core/Inc/config.h
@@ -30,32 +30,36 @@
 #define LED8_PIN 					GPIO_PIN_11
 
 /*
- * UART 1 ET UART2 (printf) NUCLEO
+ * UART3 ET UART7 (printf) NUCLEO
  */
 
-#define USE_UART1								0
-#define UART1_BAUDRATE							115200
-
-#define USE_UART2								0
-#define UART2_BAUDRATE 							115200
-
-#define USE_UART3								1			//USART3_TX PB10 (PIN8)    USART3_RX PB12 (PIN10)
+#define USE_UART3								0			//USART3_TX PB10 (PIN8)    USART3_RX PB12 (PIN10)
 #define UART3_BAUDRATE 							115200
 
-#define USE_UART4								0
-#define UART4_BAUDRATE 							115200
-
-#define USE_UART5								0
-#define UART5_BAUDRATE 							115200
-
-#define USE_UART6								0
-#define UART6_BAUDRATE 							115200
-
-#define USE_UART7								0		  //UART7_TX PE8 (D1)  UART7_RX PE7 (D0)
+#define USE_UART7								1		  //UART7_TX PE8 (D1)  UART7_RX PE7 (D0)
 #define UART7_BAUDRATE 							115200
 
-#define USE_UART8								0
-#define UART8_BAUDRATE 							115200
+
+/*
+ * I2C
+ */
+#define I2C_TIMEOUT	1000
+
+#define I2C5_SDA_GPIO							GPIOA
+#define I2C5_SDA_PIN							GPIO_PIN_12
+
+#define I2C5_SCL_GPIO							GPIOA
+#define I2C5_SCL_PIN							GPIO_PIN_11
+
+/*
+ * MPU6050 I2C
+ */
+
+#define MPU6050_I2C								I2C5
+
+/*
+ * MOTORS
+ */
 
 
 #define ENABLE_MOTOR_NEGATIVE			GPIOE, GPIO_PIN_7 //D0
@@ -65,6 +69,12 @@
 
 #define PULSE_MOTOR_LEFT_NEGATIVE		GPIOD, GPIO_PIN_14 //D3 T4_CH3
 #define PULSE_MOTOR_RIGHT_NEGATIVE		GPIOD, GPIO_PIN_15 //D5 T4_CH4
+
+/*
+ * BATTERY
+ */
+
+#define BAT_ENABLE						GPIOD, GPIO_PIN_1		//D7
 
 
 #endif /* INC_CONFIG_H_ */
