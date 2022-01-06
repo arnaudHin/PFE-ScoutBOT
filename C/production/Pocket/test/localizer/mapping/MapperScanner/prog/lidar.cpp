17eff1c166e631a42d45e3fb2c68c255c980076d
Nathan BRIENT <nathan.brient@reseau.eseo.fr>
2022-01-06 13:55:23 +0100

    [CREAT] ajout test

---
+++ C/production/Pocket/test/localizer/mapping/MapperScanner/prog/lidar.cpp
@@ -0,0 +1,107 @@
+#include "lidar.hpp"
+
+#ifndef _countof
+#define _countof(_Array) (int)(sizeof(_Array) / sizeof(_Array[0]))
+#endif
+
+ofstream myfile;
+#define displayInformation 1
+#define PI 3.14159265
+
+bool checkRPLIDARHealth(RPlidarDriver * drv)
+{
+    u_result     op_result;
+    rplidar_response_device_health_t healthinfo;
+
+    op_result = drv->getHealth(healthinfo);
+    if (IS_OK(op_result)) { // the macro IS_OK is the preperred way to judge whether the operation is succeed.
+        #if displayInformation
+        printf("RPLidar health status : %d\n", healthinfo.status);
+        #endif
+        
+        if (healthinfo.status == RPLIDAR_STATUS_ERROR) {
+            fprintf(stderr, "Error, rplidar internal error detected. Please reboot the device to retry.\n");
+            // enable the following code if you want rplidar to be reboot by software
+            // drv->reset();
+            return false;
+        } else {
+            return true;
+        }
+
+    } else {
+        fprintf(stderr, "Error, cannot retrieve the lidar health code: %x\n", op_result);
+        return false;
+    }
+}
+
+
+u_result displayValue(RPlidarDriver * drv)
+{
+    u_result ans;
+    
+    rplidar_response_measurement_node_hq_t nodes[8192];
+    size_t   count = _countof(nodes);
+    
+    float valueA;
+    float valueD;
+    float valueQ;
+    float valueAR;
+    float x = 0;
+    float y = 0;
+
+    // fetech extactly one 0-360 degrees' scan
+    ans = drv->grabScanDataHq(nodes, count);
+    if (IS_OK(ans) || ans == RESULT_OPERATION_TIMEOUT) {
+        drv->ascendScanData(nodes, count);
+
+        int xBuffer[(int)count];
+        int yBuffer[(int)count];
+
+        for (int posi = 0; posi < (int)count ; ++posi) {
+
+            valueA = (nodes[posi].angle_z_q14* 90.f / (1 << 14));
+            valueD = nodes[posi].dist_mm_q2/4.0f;
+            valueQ = (nodes[posi].quality);
+
+            //convert degree to radian
+            valueA = (valueA*(PI/180.0));
+
+            //create position (x,y)
+            x = (valueD * cos(valueA));
+            y = (valueD * sin(valueA));
+
+            xBuffer[posi] = x;
+            yBuffer[posi] = y;
+        }     
+        
+        //display data on file.txt
+
+        for (int posi = 0; posi < (int)count ; ++posi) {
+            myfile << xBuffer[posi] << "," << yBuffer[posi] << ",";
+        }
+
+        fprintf(stderr, "\nFichier 'position_gtk.txt' cree\n");
+    } 
+    else 
+    {
+        printf("error code: %x\n", ans);
+    }
+    return ans;
+}
+
+void delay(_word_size_t ms){
+    while (ms>=1000){
+        usleep(1000*1000);
+        ms-=1000;
+    };
+    if (ms!=0)
+        usleep(ms*1000);
+}
+
+void fichier_open(void){
+    myfile.open("position_gtk.txt",ios_base :: app );
+}
+
+void fichier_close(void){
+    myfile.close();
+}
---
+++ C/production/Pocket/test/localizer/mapping/MapperScanner/prog/main.py
@@ -0,0 +1,63 @@
+import math
+from tkinter import *
+
+def lire_file(fichier, encodage):
+    mots = []
+    with open (fichier,encoding=encodage) as f:
+        data = f.readline()
+        mots = data.split(",")
+    return mots
+
+
+def affichage(pair,impair):
+    # créer une fenêtre avec un canevas
+    win = Tk()
+    win.title("MAP_LIDAR")
+    win.geometry("800x800")
+    can1 = Canvas(win, width = 800, height = 800, bg = 'grey')
+    can1.pack()
+    x = 400
+    y = 400
+    DIM = 10
+    #centre du repère
+    can1.create_oval(x-DIM, y-DIM, x+DIM, y+DIM,fill='yellow')
+    can1.create_line(400,0,400,800, fill = 'blue')
+    can1.create_line(0,400,800,400, fill = 'blue')
+
+    x = 400
+    y = 400
+    GRAD = 25
+    #graduation 
+    for k in range(0,16):
+        can1.create_line(x+(k*GRAD),y-DIM,x+(k*GRAD),y+DIM, fill = 'blue')
+        can1.create_line(x-(k*GRAD),y-DIM,x-(k*GRAD),y+DIM, fill = 'blue')
+        can1.create_line(x-DIM,y+(k*GRAD),x+DIM,y+(k*GRAD), fill = 'blue')
+        can1.create_line(x-DIM,y-(k*GRAD),x+DIM,y-(k*GRAD), fill = 'blue')
+
+    can1.create_text(40,20,text="| 25 cm |")
+
+    DIM = 3
+    for i in range (len(pair)):
+        x = (int(pair[i])/10) + 400
+        y = (int(impair[i])/10) + 400
+        can1.create_oval(x-DIM, y-DIM, x+DIM, y+DIM, fill='red')
+    win.mainloop()
+
+
+#programme principal
+
+data = lire_file("../output/position_gtk.txt", 'utf_8')
+x = data[0::2]
+x.pop()
+y = data[1::2]
+
+affichage(x,y)
+
+
+
+
+
+
+
+
+
